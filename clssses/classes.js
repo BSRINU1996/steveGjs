9b24b320406f11fce61219455c11533a84e7c8c6
Srinu Bali <balisrinu313@gmail.com>
2022-11-25 21:44:08 +0530

    by srin clases

---
+++ clssses/classes.js
@@ -0,0 +1,64 @@
+
+// class Jsdev {
+//     constructor(company) {
+//       this.name = 'srinu.bali';
+//       //console.log(company);
+//       this.company = company;
+//     }
+    
+//   }
+
+  class Person {
+    constructor(name, age, company) {
+      this.name = name;
+      this.age = age;
+      this.company = company;
+  }
+}
+
+const karthik =new Person("Karthik",77,"Infosys");
+console.log(karthik);
+  
+//    const developer = new Jsdev("globallogic");
+//    const developer2 = new Jsdev("infosys");
+//    const developer3 = new Jsdev("TCS");
+//    const developer4 = new Jsdev("Deloitte");
+
+
+  
+//   // console.log(developer.name);
+// //console.log(new Jsdev());
+// console.log(developer);
+// console.log(developer2);
+// console.log(developer3);
+// console.log(developer4);
+
+
+/*
+
+
+class Vehicle  {
+    constructor(_wheels){
+        this.name = Vehicle;
+      this.numberofwheels =  _wheels;
+      console.log(_wheels);
+    }
+}
+let car = new Vehicle(4);
+console.log("car")
+console.log(car);
+let bike =new Vehicle(2)
+console.log("bike")
+console.log(bike);
+let train = new Vehicle(100);
+train.coaches = 30;
+console.log("train")
+console.log(train);
+let autorikshaw = new Vehicle(3);
+console.log("autorikshaw")
+console.log(autorikshaw);
+autorikshaw._wheels = 1;
+autorikshaw.weight = 100;
+
+console.log(autorikshaw);
+*/
---
+++ nodeL&objM/index.js
@@ -0,0 +1,94 @@
+console.log("hello, good morning!");
+let arr = [ 1,2,3,4,5,6,7,8,9,'srinu',"madhu", "bali"];
+let ele = arr.at(9);
+console.log(ele);
+
+let arr1 = [89,45,37];
+let arr3 = arr1+arr
+console.log(arr3);
+document.write(arr3)
+console.log(typeof arr3)
+console.log(arr3.at(-7));
+str = "srinu";
+console.log(str.at(-4))
+
+//The join() method also joins all array elements into a string.
+
+//It behaves just like toString(), but in addition you can specify the separator:
+const fruits  = ["apple", "orange", "banana"];
+console.log(fruits.join("@"));
+console.log(fruits.toString())
+let Arr = [1,2,3,4,5,6];
+console.log(Arr.toString())
+
+//object methods
+let westeros = {
+    cersei: 'Lannister',
+    arya: 'Stark',
+    jon: 'Snow',
+    brienne: 'Tarth',
+    daenerys: 'Targaryen',
+    theon: 'Greyjoy',
+    jorah: 'Mormont',
+    margaery: 'Tyrell',
+    sandor: 'Clegane',
+    samwell: 'Tarly',
+    ramsay: 'Bolton'
+}
+let keysArr = Object.keys(westeros);
+console.log(keysArr);
+console.log( typeof keysArr);
+let valArr = Object.values(westeros);
+console.log(valArr);
+console.log(typeof valArr);
+let nestobj = Object.entries(westeros)
+console.log(nestobj);
+console.log(nestobj[0]);
+console.log(nestobj[6]);
+console.log(nestobj[4].length);
+
+
+//NODELIST................
+
+
+const myNodelist = document.querySelectorAll("p");
+
+ document.getElementById("demo").innerHTML = "The innerHTML of the second paragraph is: " + myNodelist[4].innerHTML;
+console.log(myNodelist);
+console.log(myNodelist[4]);
+console.log(myNodelist[3])
+console.log(myNodelist[2]);
+console.log(myNodelist[1]);
+console.log(myNodelist[0]);
+console.log(myNodelist.length);
+for(let i= 0 ; i<myNodelist.length ; i++){
+    if (i > 3){
+        myNodelist[i].style.color = "red";
+
+    }
+    else{
+        myNodelist[i].style.color = "green";
+
+    }
+}
+/*
+A NodeList may look like an array, but it is not.
+
+You can loop through a NodeList and refer to its nodes by index.
+
+But, you cannot use Array methods like push(), pop(), or join() on a NodeList.
+
+*/
+// console.log([1,2,3,4,5].pop());
+
+// console.log([1,2,3,4].push(2));
+
+
+//classes
+//Constructot function
+
+function Javascript(methods){
+    this.methods = "arraymethods"
+}
+let jsm =  new Javascript("unshiftmethod");
+console.log(jsm);
